0812c859a444795fadbe4739e04bb427e5cc2761
Koen Geusens <koen_geusens@hotmail.com>
2023-04-26 08:45:09 +0200

    Patch to support vuetify and mdi icons

--- babylon/src/App.vue
+++ babylon/src/App.vue
@@ -1,12 +1,16 @@
 <template>
+  <PuzzleBrowser />
   <BabylonScene />
 </template>
 
 <script>
 import BabylonScene from "./components/BabylonScene.vue";
+import PuzzleBrowser from "./components/PuzzleBrowser.vue"
+
 export default {
   name: "App",
   components: {
+    PuzzleBrowser,
     BabylonScene,
   },
 };
---
+++ babylon/src/components/PuzzleBrowser.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1> Hello World {{ selectedFile }} </h1>
+  <div>
+  <v-btn prepend-icon="mdi-vuetify" @click="loadFileList">
+    Load Files 
+  </v-btn>
+  <v-card width="400">
+    <v-combobox margin:10 v-model="selectedFile"
+      label = "Puzzle file"
+      :items="fileList"
+      >
+    </v-combobox>
+  </v-card>
+</div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const fileList = ref([])
+  const selectedFile = ref('')
+  function loadFileList() {
+    const res = fetch('http://localhost:3001/api/puzzles/list').then(
+      res => res.json()).then(obj => {fileList.value = obj;}) 
+  }
+</script>
