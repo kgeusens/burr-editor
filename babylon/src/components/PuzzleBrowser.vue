044be7253ba96f54a56a5bb577f1f64e2a71ccf1
Koen Geusens <koen_geusens@hotmail.com>
2023-04-27 10:29:58 +0200

    upgrade burr-data to v0.1.12

--- babylon/src/components/PuzzleBrowser.vue
+++ babylon/src/components/PuzzleBrowser.vue
@@ -13,13 +13,24 @@
     <v-btn v-if="fileList.includes(selectedFile)" @click="loadFile">
       Load File
     </v-btn>
+    <v-card v-if="loadedFile.puzzle">
+      <v-list>
+        <v-list-subheader>{{ loadedFile.name }}</v-list-subheader>
+        <v-list-item  v-for="(item, i) in loadedFile.puzzle.shapes.voxel" :key="i" :value="item" @click="selectShape(item)">
+          <template v-slot:prepend>{{ i }}</template>
+          <v-list-item-title v-text="item.name"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card>
   </v-card>
 </div>
 </template>
 
 <script setup>
   import { reactive, computed } from 'vue'
-  const DATA= reactive({fileList: [], selectedFile: '', loadedFile: {} })
+  import { Puzzle, Voxel } from "@kgeusens/burr-data"
+
+  const DATA= reactive({fileList: [], selectedFile: '', loadedFile: {}, selectedShape: {} })
   function loadFileList() {
     selectedFile.value = ''
     const res = 
@@ -29,12 +40,18 @@
       .catch(error => console.log(error))
   }
   function loadFile() {
+    selectedShape.value = {}
     const res = 
       fetch('http://localhost:3001/api/puzzles/get/' + selectedFile.value, { mode: "cors" })
       .catch(error => console.log(error))
-      .then(res => res.json()).then(obj => {console.log(obj)})
-      .catch(error => console.log(error))
+      .then(res => res.json())
+      .then(obj => {
+        loadedFile.value.name = obj.filename
+        loadedFile.value.puzzle = new Puzzle(obj.content.puzzle)
+        selectedShape.value = {}
+      }).catch(error => console.log(error))
   }
+  function selectShape(p) { selectedShape.value = p }
   const fileList = computed({
     get: () => DATA.fileList, 
     set: (val) => DATA.fileList = val
@@ -42,6 +59,14 @@
   const selectedFile = computed({
     get: () => DATA.selectedFile, 
     set: (val) => DATA.selectedFile = val
+  })
+  const loadedFile = computed({
+    get: () => DATA.loadedFile, 
+    set: (val) => DATA.loadedFile = val
+  })
+  const selectedShape = computed({
+    get: () => DATA.selectedShape,
+    set: (val) => DATA.selectedShape = val
   })
 
 </script>
