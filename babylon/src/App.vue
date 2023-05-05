c0557ee39f298ad61575839f0bdfd5e6cb91565a
Koen Geusens <koen_geusens@hotmail.com>
2023-05-05 11:45:26 +0200

    conditional v-show on main screen

--- babylon/src/App.vue
+++ babylon/src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar extension-height="48" density="compact">
+    <v-app-bar density="compact">
       <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>
         Burr Editor
       </v-toolbar-title>
       <template v-slot:extension>
-        <v-tabs mandatory color="purple" height=32 v-model="tab" align-tabs="title">
-          <v-tab key="1" value="puzzle">
+        <v-tabs color="purple" v-model="tab" align-tabs="title">
+          <v-tab key="1" value="puzzle" :border="tab == 'puzzle'" >
             Puzzle
           </v-tab>
-          <v-tab key="2" value="problems">
+          <v-tab key="2" value="problems" :border="tab == 'problems'">
             Problems
           </v-tab>
-          <v-tab key="3" value="solution">
+          <v-tab key="3" value="solutions" :border="tab == 'solutions'">
             Solution
           </v-tab>
         </v-tabs>
       </template>
     </v-app-bar>
-    <v-navigation-drawer class="pa-2"
+
+    <v-main v-show="tab == 'puzzle'">
+      <v-navigation-drawer 
+        class="pa-2"
         v-model="drawer"
         temporary
-      >
+        >
         <PuzzleBrowser @newShape="loadShape"/>
-    </v-navigation-drawer>
-    <v-main id="main">
+      </v-navigation-drawer>
       <v-layout-item model-value position="top" class="text-start" size="0">
         <div class="ma-4">
           <v-btn @click.stop="drawer = !drawer" icon="mdi-arrow-right" size="small" elevation="4" />
@@ -42,29 +44,24 @@
             </BabylonCamera>
           </BabylonScene>
         </BabylonEngine>
-<!--        
-        <BabylonMainScene height="parent" width="parent" :shape="shape"/>
--->
       </v-card>
     </v-main>
   </v-app>
 </template>
 
 <script>
-//import BabylonMainScene from "./components/BabylonMainScene.vue";
-import { sceneBuilder } from "./scenes/ShapeEditor.js";
 import PuzzleBrowser from "./components/PuzzleBrowser.vue"
 import BabylonEngine from "./components/BabylonEngine.vue";
 import BabylonScene from "./components/BabylonScene.vue";
 import BabylonSceneModel from "./components/BabylonSceneModel.vue";
 import BabylonCamera from "./components/BabylonCamera.vue";
 import BabylonView from "./components/BabylonView.vue";
-// import { Voxel } from "@kgeusens/burr-data"
+import { sceneBuilder } from "./scenes/ShapeEditor.js";
 
 export default {
   name: "App",
   data() {
-    return { shape: {}, VoxelEditor: sceneBuilder, drawer: false, rail: true, tab: "puzzle" }
+    return { puzzle: {}, shape: {}, VoxelEditor: sceneBuilder, drawer: false, rail: true, tab: null }
   },
   components: {
     PuzzleBrowser,
@@ -81,9 +78,9 @@ export default {
   watch:{
     shape(newv, oldv) { 
     },
-    tab(newv, oldv) { console.log(newv)}
   },
   mounted() { 
   }
 };
 </script>
+
