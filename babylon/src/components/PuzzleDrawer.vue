fd71a022ab4f5eaa9a724ce3d504b91218ada121
Koen Geusens <koen_geusens@hotmail.com>
2023-05-14 15:45:33 +0200

    use click.stop for delete button

--- babylon/src/components/PuzzleDrawer.vue
+++ babylon/src/components/PuzzleDrawer.vue
@@ -1,6 +1,13 @@
 <template>
   <v-card variant="outlined" v-if="props.puzzle.shapes">
-    <v-toolbar density="compact" color="primary" :title="`${props.puzzle.shapes.voxel.length} Shapes`"></v-toolbar>
+    <v-toolbar density="compact" color="primary">
+      <v-toolbar-title>
+        {{ props.puzzle.shapes.voxel.length }} Shapes <v-spacer></v-spacer> 
+      </v-toolbar-title>
+      <v-btn icon @click="addShape">
+        <v-icon>mdi-playlist-plus</v-icon>
+      </v-btn>
+    </v-toolbar>
     <v-card class="overflow-y-auto" max-height="400"  >
       <v-list mandatory v-model:selected="DATA.selectedItem">
         <v-list-item  v-for="(item, i) in shapes" class="py-0 px-1" :key="i" :value="i">
@@ -25,20 +32,17 @@
             >
             </v-text-field></v-col>
             <v-col class = "pa-0" align="right">
-              <v-icon v-if="DATA.selectedItem[0] == i" @click="deleteShape" icon="mdi-delete"></v-icon>
+              <v-icon v-if="DATA.selectedItem[0] == i && props.puzzle.shapes.voxel.length > 1" @click.stop="deleteShape" icon="mdi-delete"></v-icon>
             </v-col>
           </v-row></v-container></v-list-item-title>
         </v-list-item>
       </v-list>
-      <v-card-actions><v-spacer></v-spacer>
-        <v-btn @click="addShape">New</v-btn>
-      </v-card-actions>
     </v-card>
   </v-card>
 </template>
 
 <script setup>
-  import { reactive, computed, watch } from 'vue'
+  import { reactive, computed, watch, nextTick } from 'vue'
   import { Puzzle, Voxel } from "@kgeusens/burr-data"
 
   const emit = defineEmits(["newShape"])
@@ -67,7 +71,7 @@
 
   const selectedIndex = computed({
     get: () => DATA.selectedItem[0],
-    set: (val) => DATA.selectedItem[0] = val
+    set: (val) => DATA.selectedItem = [val]
   })
 
   const shapes = computed({
