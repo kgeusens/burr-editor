7bbfdd5a5f985627c2bbb71942df557971824fb1
Koen Geusens <koen_geusens@hotmail.com>
2023-05-20 10:10:57 +0200

    added groundColor to light

--- babylon/src/components/babylon/BabylonScene.vue
+++ babylon/src/components/babylon/BabylonScene.vue
@@ -4,7 +4,7 @@
 
 <script setup>
   import { onMounted, inject, provide } from "@vue/runtime-core";
-  import { Scene, HemisphericLight, DirectionalLight, Vector3 } from "@babylonjs/core";
+  import { Scene, HemisphericLight, DirectionalLight, Vector3, Color3 } from "@babylonjs/core";
 
   const theEngine = inject("engine")
   const myScene = new Scene(theEngine)
@@ -14,6 +14,7 @@
   // Let there be light in this scene
   const myLight1 = new HemisphericLight("light1", new Vector3(0, 0, 1), myScene);
   myLight1.intensity = 0.5;
+  myLight1.groundColor = new Color3(1,1,1)
 //  const myLight2 = new DirectionalLight("light2", new Vector3(-0.2, 1, -1), myScene);
 //  myLight2.intensity = 1;
 
