200dc26121e1c67d3e258c10f615e254351776d2
Koen Geusens <koen_geusens@hotmail.com>
2023-05-20 14:28:27 +0200

    lighting changes

--- babylon/src/components/babylon/BabylonScene.vue
+++ babylon/src/components/babylon/BabylonScene.vue
@@ -14,9 +14,9 @@
   // Let there be light in this scene
   const myLight1 = new HemisphericLight("light1", new Vector3(0, 0, 1), myScene);
   myLight1.intensity = 0.5;
-  myLight1.groundColor = new Color3(1,1,1)
-//  const myLight2 = new DirectionalLight("light2", new Vector3(-0.2, 1, -1), myScene);
-//  myLight2.intensity = 1;
+  myLight1.groundColor = new Color3(0.5,0.5,0.5)
+  const myLight2 = new DirectionalLight("light2", new Vector3(-0.2, 1, -0.5), myScene);
+  myLight2.intensity = 0.5;
 
   provide("scene", myScene)
 
